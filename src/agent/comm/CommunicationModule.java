405afabe839cd7ab6c0b554dbb317a59cb703fb2
Antons Rebguns <arebgun@gmail.com>
2006-03-19 10:02:13 +0000

    renaming CommunicationModule to CommModule

--- src/agent/comm/CommunicationModule.java
+++ src/agent/comm/CommunicationModule.java
@@ -6,12 +6,11 @@ import config.ConfigAgent;
  * @author Anton Rebgun
  * @author Dimitri Zarzhitsky
  */
-
-public abstract class CommunicationModule
+public abstract class CommModule
 {
     protected ConfigAgent agentConfig;
 
-    public CommunicationModule( ConfigAgent config )
+    protected CommModule( ConfigAgent config )
     {
         agentConfig = config;
     }
